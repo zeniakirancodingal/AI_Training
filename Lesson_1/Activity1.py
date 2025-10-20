0341196fb5881363e0c30419bfa1449fb376e8ad
Zenia Kiran <zeniakiran.codingal@gmail.com>
2025-10-20 18:42:34 +0500

    activity 1 from lesson 1

---
+++ Lesson_1/Activity1.py
@@ -0,0 +1,23 @@
+# Hello AI
+
+print("Hello! I am AI Chat Bot. What is your name?")
+
+name = input()
+
+print(f"Nice to meet you {name}!")
+
+mood = print("How are you feeling today ? Good or Bad :")
+
+mood = input().lower()
+
+if mood == 'good': 
+    print("That's good to hear!")
+
+elif mood == 'bad':
+    print("That's okay. Ups and downs are part of life.")
+
+else:
+    print("That's fine. Sometimes it's hard to decribe your feelings.")
+
+print("It was nice chatting with you. Take care!")
+
